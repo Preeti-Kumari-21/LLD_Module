bafe8a4cf43e031ae43ddcb0394e41fd76c286f8
Preeti-Kumari-21 <preeti.kumari241116@gmail.com>
2025-09-24 14:21:09 +0530

    First Commit In Streams and Lambdas

---
+++ LLD_StreamsAndLambdas/StreamsAndLambdas/src/main/java/com/scaler/streamsandlambdas/StreamsAndLambdasApplication.java
@@ -0,0 +1,13 @@
+package com.scaler.streamsandlambdas;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class StreamsAndLambdasApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(StreamsAndLambdasApplication.class, args);
+    }
+
+}
---
+++ LLD_StreamsAndLambdas/StreamsAndLambdas/src/test/java/com/scaler/streamsandlambdas/StreamsAndLambdasApplicationTests.java
@@ -0,0 +1,13 @@
+package com.scaler.streamsandlambdas;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class StreamsAndLambdasApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
