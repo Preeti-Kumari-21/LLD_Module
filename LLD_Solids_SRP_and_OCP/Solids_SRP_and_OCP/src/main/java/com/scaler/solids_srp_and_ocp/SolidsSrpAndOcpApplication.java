9be682f05d598b461e00cd41787cc3db658ecd64
Preeti-Kumari-21 <preeti.kumari241116@gmail.com>
2025-09-25 15:31:50 +0530

    First Commit for SRP and OCP

---
+++ LLD_Solids_SRP_and_OCP/Solids_SRP_and_OCP/src/main/java/com/scaler/solids_srp_and_ocp/SolidsSrpAndOcpApplication.java
@@ -0,0 +1,13 @@
+package com.scaler.solids_srp_and_ocp;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SolidsSrpAndOcpApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SolidsSrpAndOcpApplication.class, args);
+    }
+
+}
---
+++ LLD_Solids_SRP_and_OCP/Solids_SRP_and_OCP/src/test/java/com/scaler/solids_srp_and_ocp/SolidsSrpAndOcpApplicationTests.java
@@ -0,0 +1,13 @@
+package com.scaler.solids_srp_and_ocp;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SolidsSrpAndOcpApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
